e768c831c96ff0028f161e115cdddcc07a69029a
Ugonz86 <ugonzalez86@gmail.com>
2022-05-09 21:21:05 -0700

    Initial commit. Added project skeleton and basic UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import react, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import Button from '@mui/material/Button';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Fun with AI</h1>
+      <Paper className="paper">
+        <TextField 
+          onSubmit={onSubmit}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        id="outlined-basic"
+        multiline
+        fullWidth
+        label="Enter Prompt"
+        variant="outlined" />
+      <Button type="subit" className="button" variant="contained" color="primary">Contained</Button>
+      </Paper>
+      
+      <Paper className="paper2">
+        <h2>Responses</h2>
+        <Card className="card">
+          <p>{input}</p>
+          <p>{result}</p>
+        </Card>
+      </Paper>
     </div>
   );
 }
